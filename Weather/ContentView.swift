956db606314a9ebd8064878dcfb3e4eacab337a4
Chris VL <crisvladimir32@gmail.com>
2024-11-14 17:54:16 -0600

    start to create the UI

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -10,81 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .edgesIgnoringSafeArea(.all)
+            
             VStack {
-                Text("Cupertino, CA")
-                    .font(.largeTitle)
+                Text("Ciudad de México")
+                    .font(.system(size: 30))
                     .fontWeight(.semibold)
                     .foregroundStyle(.white)
-                    .padding(.bottom, 70)
+                    .padding()
                 
                 VStack {
-                    Image(systemName: "cloud.sun.rain")
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
                         .resizable()
+                        .scaledToFit()
                         .frame(width: 200, height: 200)
-                        .foregroundStyle(.white)
-                        .padding(.bottom, 40)
-                    
-                }
-                
-                VStack {
-                    Text("80°F")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
                         .padding()
                 }
                 
-                HStack {
-                    WeatherDays()
-                        .padding(.horizontal, 10)
-                    
-                    WeatherDays()
-                    
-                    WeatherDays()
-
-                    WeatherDays()
-                    
-                    WeatherDays()
-
-                }
-                
                 Spacer()
-                
-                Button(action: {}) {
-                    Text("See More")
-                        .font(.headline)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                }
             }
-            .padding()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.cyan.opacity(0.9))
     }
 }
 
 #Preview {
     ContentView()
-}
-
-struct WeatherDays: View {
-    var body: some View {
-        VStack {
-            Text("TUE")
-                .font(.callout)
-                .fontWeight(.semibold)
-                .foregroundStyle(.white)
-            
-            Image(systemName: "cloud.sun.rain")
-                .resizable()
-                .frame(width: 50, height: 50)
-                .foregroundStyle(.white)
-            
-            Text("80°F")
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .foregroundStyle(.white)
-        }
-    }
 }
